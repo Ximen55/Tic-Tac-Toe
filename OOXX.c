15888ffb5371d9a3b6dd9f2ad85e6aefae1ded37
Ximen55 <47807825+Ximen55@users.noreply.github.com>
2023-02-06 13:18:07 +0800

    Update OOXX.c

--- OOXX.c
+++ OOXX.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-void Ofunc(char c[3][3],int n,int m);
-void Xfunc(char c[3][3],int n,int m);
-int cfunc(char c[3][3]);
+void Ofunc(char c[3][3],int n,int m); //input O
+void Xfunc(char c[3][3],int n,int m); //input X
+int winfunc(char c[3][3]);	//judge win or lose
+
 int main(void)
 {
-	int n,m,i,j=0;//nª½m¾î 
-	char c[3][3]={(' ',' ',' '),(' ',' ',' '),(' ',' ',' ')};
+	int n,m,i,j = 0; 
+	char c[3][3] = {(' ',' ',' '),(' ',' ',' '),(' ',' ',' ')};
 	
 	printf(" | | \n");
 	printf("-+-+-\n");
@@ -13,24 +14,24 @@ int main(void)
 	printf("-+-+-\n");
 	printf(" | | \n");
 	
-	for(i=0;i<9;i++)
+	for(i = 0;i < 9;i++)
 	{	
 		
-		if(i%2==0)
+		if(i % 2 == 0)
 		{
 			Ofunc(c,n,m);	
-			if (cfunc(c))
+			if (winfunc(c))
 				break;
 		}
 		else
 		{
 			Xfunc(c,n,m);	
-			if (cfunc(c))
+			if (winfunc(c))
 				break;
 		} 
 		
 		
-		if(i==8)
+		if(i == 8)
 			printf("Tie!");
 	}
 	
@@ -39,17 +40,17 @@ int main(void)
 }
 void Ofunc(char c[3][3],int n,int m)
 {
-	printf("Place to put O:");
+	printf("Place to put O(number number):");
 	scanf("%d%d",&n,&m);
 	
-	if(c[n][m]=='O' || c[n][m]=='X')
+	if(c[n][m] == 'O' || c[n][m] == 'X')
 	{
 		printf("Place already taken, try another place.\n");
 		Ofunc(c,n,m);
 	}
-	else if ((n>=0&&n<=2)&&(m>=0&&m<=2))
+	else if ((n >= 0 && n <= 2) && (m >= 0&&m <= 2))
 	{	
-		c[n][m]='O';
+		c[n][m] = 'O';
 		
 		printf("%c|%c|%c\n",c[0][0],c[0][1],c[0][2]);
 		printf("-+-+-\n");
@@ -67,17 +68,17 @@ void Ofunc(char c[3][3],int n,int m)
 }
 void Xfunc(char c[3][3],int n,int m)
 {
-	printf("Place to put X:");
+	printf("Place to put X(number number):");
 	scanf("%d%d",&n,&m);
 	
-	if(c[n][m]=='O' || c[n][m]=='X')
+	if(c[n][m] == 'O' || c[n][m] == 'X')
 	{
 		printf("Place already taken, try another place.\n");
 		Xfunc(c,n,m);
 	}
-	else if ((n>=0&&n<=2)&&(m>=0&&m<=2))
+	else if ((n >= 0 && n <= 2) && (m >= 0 && m <= 2))
 	{ 
-		c[n][m]='X';
+		c[n][m] = 'X';
 		
 	printf("%c|%c|%c\n",c[0][0],c[0][1],c[0][2]);
 	printf("-+-+-\n");
@@ -93,84 +94,84 @@ void Xfunc(char c[3][3],int n,int m)
 		
 	return ;
 }
-int cfunc(char c[3][3])
+int winfunc(char c[3][3])
 {
-	if(c[0][0]=='O' && c[0][1]=='O' && c[0][2]=='O')
+	if(c[0][0] == 'O' && c[0][1] == 'O' && c[0][2] == 'O')
 	{
 		printf("Player O win!");
 		return 1;
 	}
-	else if(c[1][0]=='O' && c[1][1]=='O' && c[1][2]=='O')
+	else if(c[1][0] == 'O' && c[1][1] == 'O' && c[1][2] == 'O')
 	{
 		printf("Player O win!");
 		return 1;
 	}
-	else if(c[2][0]=='O' && c[2][1]=='O' && c[2][2]=='O')
+	else if(c[2][0] == 'O' && c[2][1] == 'O' && c[2][2] =='O')
 	{
 		printf("Player O win!");
 		return 1;
 	}
-	else if(c[0][0]=='O' && c[1][0]=='O' && c[2][0]=='O')
+	else if(c[0][0] =='O' && c[1][0] =='O' && c[2][0] == 'O')
 	{
 		printf("Player O win!");
 		return 1;
 	}
-	else if(c[0][1]=='O' && c[1][1]=='O' && c[2][1]=='O')
+	else if(c[0][1] == 'O' && c[1][1] == 'O' && c[2][1] == 'O')
 	{
 		printf("Player O win!");
 		return 1;
 	}
-	else if(c[0][2]=='O' && c[1][2]=='O' && c[2][2]=='O')
+	else if(c[0][2] == 'O' && c[1][2] == 'O' && c[2][2] =='O')
 	{
 		printf("Player O win!");
 		return 1;
 	}
-	else if(c[0][0]=='O' && c[1][1]=='O' && c[2][2]=='O')
+	else if(c[0][0] =='O' && c[1][1] =='O' && c[2][2] =='O')
 	{
 		printf("Player O win!");
 		return 1;
 	}
-	else if(c[0][2]=='O' && c[1][1]=='O' && c[2][0]=='O')
+	else if(c[0][2] =='O' && c[1][1] =='O' && c[2][0] =='O')
 	{
 		printf("Player O win!");
 		return 1;
 	}
-	else if(c[0][0]=='X' && c[0][1]=='X' && c[0][2]=='X')
+	else if(c[0][0] =='X' && c[0][1] =='X' && c[0][2] == 'X')
 	{
 		printf("Player X win!");
 		return 1;
 	}
-	else if(c[1][0]=='X' && c[1][1]=='X' && c[1][2]=='X')
+	else if(c[1][0] =='X' && c[1][1] =='X' && c[1][2] == 'X')
 	{
 		printf("Player X win!");
 		return 1;
 	}
-	else if(c[2][0]=='X' && c[2][1]=='X' && c[2][2]=='X')
+	else if(c[2][0] =='X' && c[2][1] =='X' && c[2][2] == 'X')
 	{
 		printf("Player X win!");
 		return 1;
 	}
-	else if(c[0][0]=='X' && c[1][0]=='X' && c[2][0]=='X')
+	else if(c[0][0] =='X' && c[1][0] =='X' && c[2][0] == 'X')
 	{
 		printf("Player X win!");
 		return 1;
 	}
-	else if(c[0][1]=='X' && c[1][1]=='X' && c[2][1]=='X')
+	else if(c[0][1] =='X' && c[1][1] == 'X' && c[2][1] == 'X')
 	{
 		printf("Player X win!");
 		return 1;
 	}
-	else if(c[0][2]=='X' && c[1][2]=='X' && c[2][2]=='X')
+	else if(c[0][2] == 'X' && c[1][2] == 'X' && c[2][2] == 'X')
 	{
 		printf("Player X win!");
 		return 1;
 	}
-	else if(c[0][0]=='X' && c[1][1]=='X' && c[2][2]=='X')
+	else if(c[0][0] == 'X' && c[1][1] == 'X' && c[2][2] == 'X')
 	{
 		printf("Player X win!");
 		return 1;
 	}
-	else if(c[0][2]=='X' && c[1][1]=='X' && c[2][0]=='X')
+	else if(c[0][2] == 'X' && c[1][1] == 'X' && c[2][0] == 'X')
 	{
 		printf("Player X win!");
 		return 1;
@@ -178,4 +179,3 @@ int cfunc(char c[3][3])
 	else
 		return 0;
 }
-
